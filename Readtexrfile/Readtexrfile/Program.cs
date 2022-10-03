0b829b6081c21309dddc95b286461712d814b90c
MDKAZEEMUDDIN <113545643+MDKAZEEMUDDIN@users.noreply.github.com>
2022-10-03 22:11:54 +0530

    Update Program.cs

--- Readtexrfile/Readtexrfile/Program.cs
+++ Readtexrfile/Readtexrfile/Program.cs
@@ -1,50 +1,49 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.IO;
-using System.Data;
 
+namespace Textfileread
+{
     internal class Program
     {
         static void Main(string[] args)
         {
-            string filepath = @"C:\ReadWriteFile\Readwrite.txt";
+            string filepath = @"..\..\text.txt";
             string file = File.ReadAllText(filepath);
             Console.WriteLine(file);
-
             //Read all the charecter from the text file
-
-           int count = File.ReadAllText(filepath).Length;
-           Console.WriteLine("Total words :"+count);
-           // Console.WriteLine(file);
-           //Name and extention of text file
-
-            FileInfo f=new FileInfo(filepath);
+            int count = File.ReadAllText(filepath).Length;
+            Console.WriteLine("Total characters are  :" + count);
+            
+            //It will give the information of file
+            FileInfo f = new FileInfo(filepath);
+            // Console.WriteLine(file);
+            //Name and extention of text file
             string f1 = f.Name;
             string f2 = f.Extension;
-            Console.WriteLine("file name: "+f1);
-            Console.WriteLine("extension"+f2);
+            Console.WriteLine("file name: " + f1);
+            Console.WriteLine("extension" + f2);
+            //It will just split the sentence
+            string[] a = file.Split(' ');
             //count of wordtextfile
-
-            int wordcount = 0;
-            for(int i = 0; i < file.Length - 1; i++)
+            int wordCount = 0;
+             foreach (string str in a)
             {
-                if (file[i]==' '&& char.IsLetter(file[i + 1]) && (i > 0))
-                    {
-                    wordcount++;
+                if (str != string.Empty)
+                {
+                    wordCount = wordCount + 1;
                 }
             }
-            Console.WriteLine("the total of wordcount: "+wordcount);
-
+            Console.WriteLine("\n Word Count is : " + wordCount);
             Console.ReadLine();
-           // File.Delete(@"C:\ReadWriteFile\Readwrite.txt");
-
-          // copy text file from another folder where you copied
-           string file1= @"C:\ReadWriteFile\Readwrite.txt";
-           string destination = "C:Filename1";
-            File.Copy(file1,destination,true);
-        
+           // File.Delete(@"..\..\text.txt");
+            // copy text file from another folder where you copied
+            string file1 = @"..\..\text.txt";
+            string destination = @"..\..\textcopy.txt";
+            File.Copy(file1, destination, true);
+         }
     }
 }
